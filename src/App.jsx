3063eb072f56a4b67fe2b5f9cff6f952fb3415e8
danxguerrero <danxguerrero@yahoo.com>
2025-11-09 18:07:49 -0500

    Set up boilerplate code for testimonial carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+import { TestimonialItem } from './components/testimonialItem.jsx'
+import testimonials from './data/testimonials.jsx'
 
-function App() {
-
-
+const App = () => {
+   const testimonialItems = testimonials.map((testimonial, idx) => <TestimonialItem key={idx} testimonial={testimonial} />)
   return (
-    <>
-      <h1 className="text-red-500">React Formula</h1>
-    </>
-  )
-}
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="m-12 text-4xl border-b-4 border-orange-500 p-3">Customers Love AI's Autos</h1>
+      <div className="border border-red-500 w-full max-w-5xl flex justify-between">
+        <button>Previous</button>
+        <div className="flex">{testimonialItems}</div>
+        <button>Next</button>
+      
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
