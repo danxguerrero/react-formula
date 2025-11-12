8ac1d7e55e8d01e393484871258e8d0230f2eaf2
danxguerrero <danxguerrero@yahoo.com>
2025-11-12 16:14:50 -0500

    Implement pagination for posts

--- src/pagination/Pagination.jsx
+++ src/pagination/Pagination.jsx
@@ -1,12 +1,22 @@
+import { useState } from 'react'
+
 import posts from './data/posts.jsx'
 import { Post }from './components/Post.jsx'
+import { PageButton } from './components/PageButton.jsx'
 
 export const Pagination = () => {
-    const postItems = posts.map((post, idx) => <Post key={idx} post={post}/>)
+    const [pageIdx, setPageIdx] = useState(0)
+    const pageCount = Math.ceil(posts.length / 9)
+    const pageMap = Array.from({length: pageCount}).map((_, idx) => <PageButton key={idx} onClick={() => setPageIdx(idx)} pageNum={idx}/>)
+
+    const postItems = posts.slice(9 * pageIdx, 9 * (pageIdx + 1)).map((post, idx) => <Post key={idx} post={post}/>)
     return (
-        <div className="flex justify-center" >
-            <div className="border border-black w-full w-full max-w-5xl flex flex-wrap">
+        <div className="flex flex-col justify-center items-center m-5" >
+            <div className="w-full w-full max-w-5xl flex flex-wrap justify-center">
                 {postItems}
+            </div>
+            <div flex className="flex justify-center items-center">
+                {pageMap}
             </div>
         </div>
     )
