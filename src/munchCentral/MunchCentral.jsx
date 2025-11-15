0c21c3cfd8fef72d49fc1ca38614181cc1be6663
danxguerrero <danxguerrero@yahoo.com>
2025-11-14 21:47:35 -0500

    Complete Munch Central exercise closes #6

--- src/munchCentral/MunchCentral.jsx
+++ src/munchCentral/MunchCentral.jsx
@@ -1,16 +1,19 @@
 import restaurants from "./data/restaurants";
-
+import { RestaurantItem } from "./components/RestaurantItem";
 
 export const MunchCentral = () => {
-    const restaurantItems = restaurants.map((restaurant, idx) => <div key={idx}>{restaurant.name}</div>);
+    const restaurantItems = restaurants.map((restaurant, idx) => <RestaurantItem key={idx} restaurant={restaurant} /> );
+
     return (
-        <div className='border border-red-500 flex justify-center items-center m-5'>
-            <div className='w-full max-w-5xl flex flex-col justify-center items-center'>
+        <div className='w-full m-4 relative'>
+            <div className='flex flex-col items-center h-full'>
                 <div className='flex items-center'>
-                <img src='https://static-task-assets.react-formula.com/996965.png'/>
-                <h1 className="text-4xl">Munch Central</h1>
+                    <img src='https://static-task-assets.react-formula.com/996965.png' className='m-3 h-24 w-24'/>
+                    <h1 className="text-4xl text-neutral-600">Munch Central</h1>
                 </div>
-                {restaurantItems}
+                <div className='w-full max-w-4xl flex flex-wrap justify-center h-full'>
+                    {restaurantItems}
+                </div>
             </div>
         </div>
     )
--- src/munchCentral/components/RestaurantItem.jsx
+++ src/munchCentral/components/RestaurantItem.jsx
@@ -1,6 +1,24 @@
-export const RestaurantItem = ({restaurant}) => {
+export const RestaurantItem = ({ restaurant }) => {
+
+    const costSymbols = "$".repeat(restaurant.cost)
 
     return (
-        <div>{restaurant.name}</div>
+        <div className='m-3 shadow-md bg-white relative'>
+            <img src={restaurant.image} className='w-56 h-48 object-cover' />
+            {restaurant?.promotion && <div className='absolute top-3 left-0 text-white text-xs bg-rose-500 px-3 py-1 rounded-r-lg'>promotion</div>}
+            <div className='p-3'>
+                <div className="flex justify-between">
+                    <div>{restaurant.name}</div>
+                    <div className='text-rose-500 '>
+                        <i className='fa-solid fa-star mr-1' />
+                        {restaurant.rating}
+                    </div>
+                </div>
+                <div className="mt-2 text-stone-500">
+                    <p>{restaurant.location}</p>
+                    <p>{costSymbols}</p>
+                </div>
+            </div>
+        </div>
     )
 }
