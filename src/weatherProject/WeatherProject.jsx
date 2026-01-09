6c6ce732c23c1d259635412256d33d44843c2b8a
danxguerrero <danxguerrero@yahoo.com>
2026-01-09 16:27:30 -0500

    Implement WeatherCard component

--- src/weatherProject/WeatherProject.jsx
+++ src/weatherProject/WeatherProject.jsx
@@ -1,3 +1,38 @@
+import { useState, useEffect } from 'react'
+
+import { WeatherCard } from './components/WeatherCard.jsx'
+
 export const WeatherProject = () => {
-    return <div>Weather Project</div>
+
+    const [forecasts, setForecasts] = useState([])
+
+    const fetchForecast = async () => {
+        try {
+            const response = await fetch("https://api.react-formula.com/learning-api/demos/weather-forecast/weather")
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`)
+            }
+
+            const data = await response.json()
+
+            setForecasts(data)
+        } catch (err) {
+            console.error("There was an error fetching forecasts: ", err)
+        }
+
+    }
+
+
+    useEffect(() => {
+        fetchForecast()
+    }, [])
+
+    const weatherCards = forecasts.map((forecast, idx) => <WeatherCard key={idx} forecast={forecast} />)
+
+    return <div className="flex items-center justify-center h-screen bg-neutral-50">
+                <div className="flex w-full justify-between max-w-3xl">
+                    {weatherCards}
+                </div>
+            </div>
 }
