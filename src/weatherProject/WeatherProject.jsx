c028d467eef982de5291596aef249d7e0bfcfe45
danxguerrero <danxguerrero@yahoo.com>
2026-01-09 17:00:02 -0500

    Implement loading spinner

--- src/weatherProject/WeatherProject.jsx
+++ src/weatherProject/WeatherProject.jsx
@@ -5,8 +5,10 @@ import { WeatherCard } from './components/WeatherCard.jsx'
 export const WeatherProject = () => {
 
     const [forecasts, setForecasts] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
 
     const fetchForecast = async () => {
+        setIsLoading(true)
         try {
             const response = await fetch("https://api.react-formula.com/learning-api/demos/weather-forecast/weather")
 
@@ -19,8 +21,9 @@ export const WeatherProject = () => {
             setForecasts(data)
         } catch (err) {
             console.error("There was an error fetching forecasts: ", err)
+        } finally {
+            setIsLoading(false)
         }
-
     }
 
 
@@ -31,16 +34,18 @@ export const WeatherProject = () => {
     const weatherCards = forecasts.map((forecast, idx) => <WeatherCard key={idx} forecast={forecast} />)
 
     return <div className="flex flex-col items-center  h-screen bg-neutral-50">
-                <div className="w-full flex justify-end p-3">
-                    <button
-                        onClick={fetchForecast}
-                        className="bg-orange-200 text-orange-500 text-xl py-3 px-8 rounded-lg hover:bg-orange-300"
-                    >
-                        <i class="fa-solid fa-rotate-right"></i>
-                    </button>
-                </div>
-                <div className="flex w-full justify-between items-center h-full max-w-3xl">
-                    {weatherCards}
-                </div>
+        <div className="w-full flex justify-end p-3">
+            <button
+                onClick={fetchForecast}
+                className="bg-orange-200 text-orange-500 text-xl py-3 px-8 rounded-lg hover:bg-orange-300"
+            >
+                <i class="fa-solid fa-rotate-right"></i>
+            </button>
+        </div>
+        {isLoading ? <div className="flex items-center justify-center h-full text-2xl"><i class="fa-regular fa-square text-orange-500 animate-spin"></i></div> :
+            <div className="flex w-full justify-between items-center h-full max-w-3xl">
+                {weatherCards}
             </div>
+        }
+    </div>
 }
