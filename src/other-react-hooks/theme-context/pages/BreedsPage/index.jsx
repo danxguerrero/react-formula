ee19ce4a3a85562808be14d1afa72934b29f802d
danxguerrero <danxguerrero@yahoo.com>
2026-01-25 17:43:50 -0500

    Complete Theme Context

---
+++ src/other-react-hooks/theme-context/pages/BreedsPage/index.jsx
@@ -0,0 +1,49 @@
+import { useState, useEffect, useContext } from "react";
+import clsx from "clsx";
+import NavBar from "../../shared/NavBar";
+import * as BreedService from "../../services/breed";
+import BreedItem from "./BreedItem";
+import ThemeContext from '../../ThemeContext'
+
+const SPINNER_STYLE = {
+  cyan: "text-cyan-500",
+  rose: "text-rose-500",
+  yellow: "text-yellow-500",
+};
+
+const BreedsPage = () => {
+  const { themeColor, setThemeColor } = useContext(ThemeContext);
+  const [breeds, setBreeds] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      setIsLoading(true);
+      const response = await BreedService.getBreeds();
+      setBreeds(await response.json());
+      setIsLoading(false);
+    })();
+  }, []);
+
+  return (
+    <>
+      <NavBar />
+      <div className="flex justify-center font-pt-sans">
+        <div className="flex flex-col items-center w-full max-w-4xl px-2 my-24">
+          {isLoading ? (
+            <i
+              className={clsx(
+                "fad fa-spinner-third animate-spin text-4xl my-20",
+                SPINNER_STYLE[themeColor]
+              )}
+            ></i>
+          ) : (
+            breeds.map((breed, idx) => <BreedItem key={idx} breed={breed} />)
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BreedsPage;
