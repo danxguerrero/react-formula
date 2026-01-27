79cc17932049554eae27dd557f5bf4211c7e790f
danxguerrero <danxguerrero@yahoo.com>
2026-01-27 14:46:46 -0500

    Remove border from Hero Section

--- src/responsive-design/mobile-responsive-design/components/HeroSection.jsx
+++ src/responsive-design/mobile-responsive-design/components/HeroSection.jsx
@@ -1,7 +1,7 @@
 export const HeroSection = () => {
     return (
         <div className="flex justify-center">
-            <div className="w-full max-w-6xl border border-red-500 flex flex-col-reverse md:flex-row items-center justify-center py-32 p-4">
+            <div className="w-full max-w-6xl flex flex-col-reverse md:flex-row items-center justify-center py-32 p-4">
                 <div className="text-violet-800 text-4xl md:text-5xl font-ubuntu font-medium max-w-[450px]">
                     <div>Something <span className="text-yellow-500">Catchy</span> and <span className="text-red-600">Technological</span></div>
                     <button className="border-4 border-violet-800 text-xl font-medium px-10 py-3 mt-4">Learn More</button>
